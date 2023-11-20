ea94032ed64d38046e7e2d01aa05113592e083a1
Sean Russell <sean@seans-mbp.lan>
2023-11-19 22:04:04 -0500

    setup frontend routing, login and session

--- router.py
+++ router.py
@@ -1,23 +1,44 @@
 import os
-from flask import Flask, send_from_directory, request
+from flask import Flask, send_from_directory, request, session, redirect, render_template
 from dotenv import load_dotenv
 from loan import *
 
 load_dotenv()  # take environment variables from .env.
 
 app = Flask(__name__)
+app.secret_key = os.environ.get('APP_KEY')
 
 cached_loan = None
 
 @app.route("/")
 def main():
-  return send_from_directory('./dist/', 'index.html')
+  if 'username' in session:
+    return send_from_directory('./dist/', 'index.html')
+  else:
+    return redirect('/login')
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+  if request.method == "GET":
+    return send_from_directory('./dist/', 'index.html')
+  elif request.method == "POST":
+    username = request.args.get("username")
+    session['username'] = request.form['username']
+    # password = request.args.get("password")
+    # session['password'] = request.form['password']
+    return redirect('/')
+
 
 @app.route("/assets/<filename>")
 def assets(filename):
   return send_from_directory('./dist/assets/', filename)
 
-@app.route("/api/loan", methods={"GET", "POST"})
+# @app.route("/lib/<filename>")
+# def lib(filename):
+#   print('FILENAME: ' + filename)
+#   return send_from_directory('./node_modules/', filename)
+
+@app.route("/api/loan", methods=["GET", "POST"])
 def api():
   if request.method == "POST":
     data = request.json
---
+++ src/Components/Dashboard.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'preact/hooks'
+import Input from './Input';
+import axios from 'axios';
+
+export default function Dashboard() {
+  const [values, setValues] = useState({});
+  const setValue = (e) => {
+    const key = e.target.name || e.target.id;
+    const value = e.target.value;
+    setValues({...values, [key]: value});
+  };
+
+  const submit = async (e) => {
+    e.preventDefault();
+    const res = await axios.post('/api/loan', values);
+    console.log(res);
+  };
+  return (
+    <form method="POST" onSubmit={submit}>
+      <Input
+        label="Title"
+        name="title"
+        isRequired={true}
+        onChange={setValue} />
+      <Input
+        label="Starting Balance"
+        name="startBalance"
+        isRequired={true}
+        onChange={setValue}
+        type="number"
+        inputMode="decimal" />
+      <Input
+        label="Interest Rate"
+        name="interestRate"
+        isRequired={true}
+        onChange={setValue}
+        type="number"
+        inputMode="decimal" />
+      <Input
+        label="Payment Amount"
+        name="paymentAmt"
+        isRequired={true}
+        onChange={setValue}
+        type="number"
+        inputMode="decimal" />
+
+      <button class="form-control">Submit</button>
+    </form>
+  );
+}
