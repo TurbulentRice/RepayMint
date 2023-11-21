78be13e13633d42f71de11c1967f580bf7208625
Sean Russell <sean@seans-mbp.lan>
2023-11-21 02:18:16 -0500

    login logout

--- router.py
+++ router.py
@@ -3,37 +3,41 @@ from flask import Flask, send_from_directory, request, session, redirect, render
 from dotenv import load_dotenv
 from app.loan import *
 
-load_dotenv()  # take environment variables from .env.
+load_dotenv()
 
 app = Flask(__name__)
 app.secret_key = os.environ.get('APP_KEY')
 
+# This is just to store Loans in session until we connect db
 @app.before_request
 def make_session_permanent():
     session.permanent = True
 
 @app.route("/")
 def main():
-  if 'username' in session:
-    return send_from_directory('./dist/', 'index.html')
-  else:
-    return redirect('/login')
+  if 'username' not in session:
+    return redirect("/login")
+  return send_from_directory('./client/dist/', 'index.html')
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-  if request.method == "GET":
-    return send_from_directory('./dist/', 'index.html')
-  elif request.method == "POST":
+  if request.method == "POST":
     username = request.args.get("username")
     session['username'] = request.form['username']
-    # password = request.args.get("password")
-    # session['password'] = request.form['password']
-    return redirect('/')
+    return redirect("/")
+  elif 'username' in session:
+    return redirect("/")
+  else:
+    return send_from_directory('./client/dist/', 'index.html')
 
+@app.route("/logout")
+def logout():
+  session.clear()
+  return redirect("/login")
 
 @app.route("/assets/<filename>")
 def assets(filename):
-  return send_from_directory('./dist/assets/', filename)
+  return send_from_directory('./client/dist/assets/', filename)
 
 @app.route("/api/loan/new", methods=["GET", "POST"])
 def new_loan():
